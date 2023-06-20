efb828f0d367be27f16b1910fd8c55558f01e10d
xuananh95 <xanh.trym95@gmail.com>
2023-06-20 08:13:47 +0700

    added webpack

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function App() {
     return (
         <div>
             <h1>Xin chào anh em F8!</h1>
+            <h1>Xin chào anh em F8!</h1>
         </div>
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js", // Dẫn tới file index.js ta đã tạo
@@ -20,5 +21,9 @@ module.exports = {
         ],
     },
     // Chứa các plugins sẽ cài đặt trong tương lai
-    plugins: [],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
 };
